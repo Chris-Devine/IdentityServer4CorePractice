ff8dfb537113dfed7188859852ef8a65560fdee0
Chris Devine <Chris Devine>
2016-12-05 07:02:54 +0000

    initial

---
+++ src/MvcClient/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+namespace MvcClient.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public IActionResult About()
+        {
+            ViewData["Message"] = "If you see this you are authorised";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+
+        public async Task Logout()
+        {
+            await HttpContext.Authentication.SignOutAsync("Cookies");
+            await HttpContext.Authentication.SignOutAsync("oidc");
+        }
+    }
+}
---
+++ src/QuickStartIdentityServer/config.cs
@@ -0,0 +1,119 @@
+﻿using IdentityServer4.Models;
+using IdentityServer4.Services.InMemory;
+using System.Collections.Generic;
+using System.Security.Claims;
+
+namespace QuickStartIdentityServer
+{
+    public class Config
+    {
+        // Scopes define the resources in your system that you want to protect, e.g. APIs.
+        public static IEnumerable<Scope> GetScopes()
+        {
+
+            return new List<Scope>
+            {
+                StandardScopes.OpenId,
+                StandardScopes.Profile,
+
+                new Scope
+                {
+                    Name = "api1",
+                    Description = "My API"
+                },
+                new Scope
+                {
+                    Name = "api1",
+                    Description = "My API"
+                }
+            };
+        }
+
+        // The next step is to define a client that can access this API.
+        // For this scenario, the client will not have an interactive user, and will authenticate using the so called client secret with IdentityServer.
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "client",
+
+                    // no interactive user, use the clientid/secret for authentication
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+
+                    // secret for authentication
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+
+                    // scopes that client has access to
+                    AllowedScopes = {"api1"}
+                },
+
+                new Client
+                {
+                    ClientId = "ro.client",
+                    AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
+
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+                    AllowedScopes = {"api1"}
+                },
+                new Client
+                {
+                    ClientId = "mvc",
+                    ClientName = "MVC Client",
+                    AllowedGrantTypes = GrantTypes.Implicit,
+
+                    // where to redirect to after login
+                    RedirectUris = { "http://localhost:5002/signin-oidc" },
+
+                    // where to redirect to after logout
+                    PostLogoutRedirectUris = { "http://localhost:5002" },
+
+                    AllowedScopes = new List<string>
+                    {
+                        StandardScopes.OpenId.Name,
+                        StandardScopes.Profile.Name
+                    }
+                }
+            };
+        }
+
+        public static List<InMemoryUser> GetUsers()
+        {
+            return new List<InMemoryUser>
+            {
+                new InMemoryUser
+                {
+                    Subject = "1",
+                    Username = "alice",
+                    Password = "password",
+
+                    Claims = new []
+                    {
+                        new Claim("name", "Alice"),
+                        new Claim("website", "https://alice.com")
+                    }
+                },
+                new InMemoryUser
+                {
+                    Subject = "2",
+                    Username = "bob",
+                    Password = "password",
+
+                    Claims = new []
+                    {
+                        new Claim("name", "Bob"),
+                        new Claim("website", "https://bob.com")
+                    }
+                }
+            };
+        }
+
+    }
+}
